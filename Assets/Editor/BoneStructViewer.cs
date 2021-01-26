b9183f8c4b93494542ed4d4618acbae7d4444006
Umin Lee <netuee@naver.com>
2021-01-26 23:58:46 +0900

    drag n drop, show structure
    오브젝트 끌어다 넣으면 하위 구조 보여줌

---
+++ Assets/Editor/BoneStructViewer.cs
@@ -0,0 +1,196 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoneStructViewer : EditorWindow
+{
+    private class ChildObjectItem
+    {
+        public Transform transform;
+        public List<ChildObjectItem> childs = new List<ChildObjectItem>();
+    }
+
+    private Queue<ChildObjectItem> objCache = new Queue<ChildObjectItem>();
+    private ChildObjectItem lootObject;
+
+    private Vector2 childScrollviewPos;
+
+    [MenuItem("CustomWindow/ChildStructureViewer")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BoneStructViewer),false,"ChildStructureViewer");
+
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space ();
+        DropAreaGUI ();
+        EditorGUILayout.BeginVertical();
+
+        HorizontalLine(new Vector2(20f, 20f));
+
+        EditorGUILayout.EndVertical();
+        GUILayout.Space(5f);
+        DrawTest();
+
+    }
+
+    void OnDestroy()
+    {
+        Dispose();
+    }
+
+    public void HorizontalLine(Vector2 margin) => HorizontalLine(Color.gray, 1f, margin);
+    public void HorizontalLine(Color color, float height, Vector2 margin)
+    {
+        GUILayout.Space(margin.x);
+
+        EditorGUI.DrawRect(EditorGUILayout.GetControlRect(false, height), color);
+
+        GUILayout.Space(margin.y);
+    }
+
+    private void DrawTest()
+    {
+        GUILayout.BeginVertical();
+        childScrollviewPos = GUILayout.BeginScrollView(childScrollviewPos);
+
+        if(lootObject != null)
+            DrawItem(lootObject,0);
+
+        GUILayout.EndScrollView();
+        GUILayout.EndVertical();
+    }
+
+    private void DrawItem(ChildObjectItem item, int deep)
+    {
+        Draw(item.transform.name,deep++);
+        foreach(var child in item.childs)
+        {
+            DrawItem(child,deep);
+        }
+    }
+
+    private void Draw(string name, int deep)
+    {
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Space((float)(deep * 10f));
+        Rect drop_area = GUILayoutUtility.GetRect(0, 20.0f,GUILayout.ExpandWidth (true));
+        GUIStyle style = new GUIStyle(GUI.skin.box);
+        style.alignment = TextAnchor.MiddleLeft;
+        GUI.Box (drop_area, name, style);
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+    }
+
+    private void SetItems(Transform loot)
+    {
+        ChildObjectItem item = GetChildObjectItemCache();
+        item.transform = loot;
+        item.childs = GetChildItems(item);
+
+        lootObject = item;
+    }
+
+    private List<ChildObjectItem> GetChildItems(ChildObjectItem loot)
+    {
+        List<ChildObjectItem> list = new List<ChildObjectItem>();
+
+        int count = loot.transform.childCount;
+        for(int i = 0; i < count; ++i)
+        {
+            var item = GetChildObjectItemCache();
+            item.transform = loot.transform.GetChild(i);
+            if(item.transform.childCount != 0)
+            {
+                item.childs = GetChildItems(item);
+            }
+
+            list.Add(item);
+        }
+
+        return list;
+    }
+
+    private void Dispose()
+    {
+        if(lootObject != null)
+            Dispose(lootObject);
+        
+        objCache.Clear();
+        objCache = null;
+    }
+
+    private void Dispose(ChildObjectItem item)
+    {
+        if(item.childs.Count != 0)
+        {
+            foreach(var child in item.childs)
+            {
+                Dispose(child);
+            }
+        }
+
+        item.transform = null;
+        item.childs.Clear();
+        item.childs = null;
+        //ReturnCache(item);
+    }
+
+    private void ReturnCache(ChildObjectItem item)
+    {
+        objCache.Enqueue(item);
+    }
+
+    private ChildObjectItem GetChildObjectItemCache()
+    {
+        if(objCache.Count == 0)
+        {
+            CreateChildObjectItem();
+        }
+
+        return objCache.Dequeue();
+    }
+
+    private void CreateChildObjectItem(int count = 1)
+    {
+        for(int i = 0; i < count; ++i)
+        {
+            objCache.Enqueue(new ChildObjectItem());
+        }
+    }
+
+    private void DropAreaGUI ()
+    {
+        Event evt = Event.current;
+        Rect drop_area = GUILayoutUtility.GetRect (0.0f, 50.0f, GUILayout.ExpandWidth (true));
+        GUI.Box (drop_area, "Add Trigger");
+     
+        switch (evt.type) {
+        case EventType.DragUpdated:
+        case EventType.DragPerform:
+            if (!drop_area.Contains (evt.mousePosition))
+                return;
+             
+            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+         
+            if (evt.type == EventType.DragPerform) {
+                DragAndDrop.AcceptDrag ();
+             
+                var transform = ((GameObject)DragAndDrop.objectReferences[0]).GetComponent<Transform>();
+                SetItems(transform);
+
+                // foreach (Object dragged_object in DragAndDrop.objectReferences) {
+                //     // Do On Drag Stuff here
+                // }
+            }
+            break;
+        }
+    }
+}
