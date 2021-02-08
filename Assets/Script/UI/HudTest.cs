7d3a13b83b1f2e0e145f953e1aac9015cf03b718
Ym39 <dpals39@gmail.com>
2021-02-08 19:53:46 +0900

    HUD 적용, ESC로 켜지는 것까지 함

--- Assets/Script/UI/HudTest.cs
+++ Assets/Script/UI/HudTest.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 public class HudTest : MonoBehaviour
 {
-    public RectTransform button1;
-    public Image button1Image;
-    public RectTransform button2;
-    public Image button2Image;
-    public RectTransform button3;
-    public Image button3Image;
-    public Vector2 startSize;
+    public ButtonHUD button1;
+    public ButtonHUD button2;
+    public ButtonHUD button3;
 
     void Start()
     {
-        button1.sizeDelta = startSize;
-        button2.sizeDelta = startSize;
-        button3.sizeDelta = startSize;
-        Color buttonColor = button1Image.color;
-        buttonColor.a = 0f;
-        button1Image.color = buttonColor;
-        button2Image.color = buttonColor;
-        button3Image.color = buttonColor;
+      
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.V))
-        {
-            button1.DOSizeDelta(new Vector2(100f, 200f), 0.5f);
-            button2.DOSizeDelta(new Vector2(100f, 200f), 0.5f);
-            button3.DOSizeDelta(new Vector2(100f, 200f), 0.5f);
-            button1Image.DOFade(0.4f, 0.5f);
-            button2Image.DOFade(0.4f, 0.5f);
-            button3Image.DOFade(0.4f, 0.5f);
-        }
+        
     }
 
+
+    public void HUDActive()
+    {
+        button3.Appear(0.2f, () => button2.Appear(0.2f, () => button1.Appear(0.2f,()=>GameManager.Instance.SwitchMenuDone())));
+
+    }
+
+    public void HUDDissable()
+    {
+        button1.Disappear(0.2f, () => button2.Disappear(0.2f, () => button3.Disappear(0.2f, () => GameManager.Instance.SwitchMenuDone())));
+
+    }
+
+    public void HUDDissable(Action action)
+    {
+        button1.Disappear(0.2f, () => button2.Disappear(0.2f, () => button3.Disappear(0.2f, () => { GameManager.Instance.SwitchMenuDone(); action(); })));
+
+    }
 }
