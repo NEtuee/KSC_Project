7b73ea24b246f7fc06bfc51938c50c31ee77415b
Umin Lee <netuee@naver.com>
2021-02-09 20:22:10 +0900

    add FMOD plugin

---
+++ Assets/ETTSSTES.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD;
+using FMODUnityResonance;
+
+public class ETTSSTES : MonoBehaviour
+{
+    public StudioEventEmitter emit;
+
+    void Start()
+    {
+        emit.EventInstance.getParameterByName("",out float value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
