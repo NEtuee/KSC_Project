1ebee97ecf0215fdba43b560e911297573c40145
Umin Lee <netuee@naver.com>
2021-02-18 15:39:55 +0900

    ik

---
+++ Assets/Script/BossSecret/IKBody.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKBody : MonoBehaviour
+{
+    public float defaultHeight = 0f;
+
+    public IKLegMovement left;
+    public IKLegMovement right;
+
+}
