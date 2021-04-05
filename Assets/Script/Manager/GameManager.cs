de6dd3408b88cd322214014c7df40cc07fe577ff
Umin Lee <netuee@naver.com>
2021-04-05 23:10:34 +0900

    asdqw

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -122,7 +122,7 @@ public class GameManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.P))
         {
-            AsynSceneManager.LoadNextlevel();
+            AsynSceneManager.LoadNextlevelFrom();
         }
     }
 
