6e50e7ab67b17f2dfccba97dd9e4163ce16158ec
Umin Lee <netuee@naver.com>
2021-04-10 22:41:33 +0900

    animation

--- Assets/Script/Boss/DaddyLongLegs/DaddyLongLegs_AI.cs
+++ Assets/Script/Boss/DaddyLongLegs/DaddyLongLegs_AI.cs
@@ -4,67 +4,67 @@ using UnityEngine;
 
 public class DaddyLongLegs_AI : IKBossBase
 {
-    public GraphAnimator animator;
-    public Transform body;
-    public Transform humanSpine;
-    public Transform graphicRoot;
+    // public GraphAnimator animator;
+    // public Transform body;
+    // public Transform humanSpine;
+    // public Transform graphicRoot;
 
-    public float startTime = 0.1f;
-    public bool randomStart = false;
-    private bool _animate = false;
+    // public float startTime = 0.1f;
+    // public bool randomStart = false;
+    // private bool _animate = false;
 
-    public void Start()
-    {
-        GetSoundManager();
-        SetLegHitGroundSound(1507);
-        if(randomStart)
-            startTime = Random.Range(0f,3f);
+    // public void Start()
+    // {
+    //     GetSoundManager();
+    //     SetLegHitGroundSound(1507);
+    //     if(randomStart)
+    //         startTime = Random.Range(0f,3f);
 
-        _timeCounter.InitTimer("Start",0f,startTime);
+    //     _timeCounter.InitTimer("Start",0f,startTime);
 
-        foreach(var leg in legs)
-        {
-            leg.Hold(true);
-        }
+    //     foreach(var leg in legs)
+    //     {
+    //         leg.Hold(true);
+    //     }
 
-        graphicRoot = transform.Find("Body");
-    }
+    //     graphicRoot = transform.Find("Body");
+    // }
 
-    public void Update()
-    {
-        if (GameManager.Instance.PAUSE == true)
-            return;
+    // public void Update()
+    // {
+    //     if (GameManager.Instance.PAUSE == true)
+    //         return;
 
-        if (!_animate)
-        {
-            _timeCounter.IncreaseTimer("Start",out var limit);
-            if(limit)
-            {
-                animator.Play("UpDownLoop",body);
-                animator.Play("HumanRotateLoop",humanSpine);
-                animator.Play("WalkRotate",body);
+    //     if (!_animate)
+    //     {
+    //         _timeCounter.IncreaseTimer("Start",out var limit);
+    //         if(limit)
+    //         {
+    //             animator.Play("UpDownLoop",body);
+    //             animator.Play("HumanRotateLoop",humanSpine);
+    //             animator.Play("WalkRotate",body);
 
-                foreach(var leg in legs)
-                {
-                    leg.Hold(false);
-                }
+    //             foreach(var leg in legs)
+    //             {
+    //                 leg.Hold(false);
+    //             }
 
-                _animate = true;
-            }
-        }
+    //             _animate = true;
+    //         }
+    //     }
         
-        var cam = Camera.main;
-        var dir = (transform.position - cam.transform.position);
-        var dist = MathEx.distance(transform.position.z, cam.transform.position.z);
-        graphicRoot.gameObject.SetActive((Vector3.Angle(cam.transform.forward,dir) <= 100f) && dist <= 150f);
+    //     var cam = Camera.main;
+    //     var dir = (transform.position - cam.transform.position);
+    //     var dist = MathEx.distance(transform.position.z, cam.transform.position.z);
+    //     graphicRoot.gameObject.SetActive((Vector3.Angle(cam.transform.forward,dir) <= 100f) && dist <= 150f);
         
-        MoveForward(frontMoveSpeed);
+    //     MoveForward(frontMoveSpeed);
 
-        if(transform.position.z <= -168.82f)
-        {
-            var pos = transform.position;
-            pos.z += 590.22f;
-            transform.position = pos;
-        }
-    }
+    //     if(transform.position.z <= -168.82f)
+    //     {
+    //         var pos = transform.position;
+    //         pos.z += 590.22f;
+    //         transform.position = pos;
+    //     }
+    // }
 }
--- Assets/Script/Boss/ImmortalJirungE/ImmortalJirungE_master.cs
+++ Assets/Script/Boss/ImmortalJirungE/ImmortalJirungE_master.cs
@@ -5,82 +5,82 @@ using UnityEngine.Events;
 
 public class ImmortalJirungE_master : MonoBehaviour
 {
-    public List<ImmortalJirungE_AI> aIs;
-    public BezierLightning lightning;
+    // public List<ImmortalJirungE_AI> aIs;
+    // public BezierLightning lightning;
 
-    public UnityEvent whenAllShieldDestroy;
+    // public UnityEvent whenAllShieldDestroy;
 
-    private int shieldCount = 0;
+    // private int shieldCount = 0;
 
-    private TimeCounterEx _timeCounterEx = new TimeCounterEx();
+    // private TimeCounterEx _timeCounterEx = new TimeCounterEx();
 
-    public void Start()
-    {
-        Recovery();
+    // public void Start()
+    // {
+    //     Recovery();
 
-        _timeCounterEx.InitTimer("time",0f,Random.Range(1f,2f));
-    }
+    //     _timeCounterEx.InitTimer("time",0f,Random.Range(1f,2f));
+    // }
 
-    public void Update()
-    {
-        if (GameManager.Instance.PAUSE == true)
-            return;
+    // public void Update()
+    // {
+    //     if (GameManager.Instance.PAUSE == true)
+    //         return;
 
-        _timeCounterEx.IncreaseTimer("time",out var limit);
-        if(limit)
-        {
-            for(int i = 1; i < aIs.Count + 1; ++i)
-            {
-                lightning.Active(aIs[i - 1].transform.position,aIs[i >= aIs.Count ? 0 : i].transform.position,3,0.1f,4f);
-            }
+    //     _timeCounterEx.IncreaseTimer("time",out var limit);
+    //     if(limit)
+    //     {
+    //         for(int i = 1; i < aIs.Count + 1; ++i)
+    //         {
+    //             lightning.Active(aIs[i - 1].transform.position,aIs[i >= aIs.Count ? 0 : i].transform.position,3,0.1f,4f);
+    //         }
 
-            _timeCounterEx.InitTimer("time",0f,Random.Range(1f,2f));
+    //         _timeCounterEx.InitTimer("time",0f,Random.Range(1f,2f));
             
-        }
+    //     }
 
-        bool whip = false;
-        foreach(var jirung in aIs)
-        {
-            if(jirung.currentState == ImmortalJirungE_AI.State.FloorWhip)
-            {
-                whip = true;
-                break;
-            }
-        }
+    //     bool whip = false;
+    //     foreach(var jirung in aIs)
+    //     {
+    //         if(jirung.currentState == ImmortalJirungE_AI.State.FloorWhip)
+    //         {
+    //             whip = true;
+    //             break;
+    //         }
+    //     }
 
-        if(!whip)
-        {
-            foreach(var jirung in aIs)
-            {
-                if(jirung.canFloorWhip && jirung.currentState == ImmortalJirungE_AI.State.WallMove)
-                {
-                    jirung.ChangeState(ImmortalJirungE_AI.State.FloorWhip);
-                    break;
-                }
-            }
-        }
+    //     if(!whip)
+    //     {
+    //         foreach(var jirung in aIs)
+    //         {
+    //             if(jirung.canFloorWhip && jirung.currentState == ImmortalJirungE_AI.State.WallMove)
+    //             {
+    //                 jirung.ChangeState(ImmortalJirungE_AI.State.FloorWhip);
+    //                 break;
+    //             }
+    //         }
+    //     }
 
         
-    }
+    // }
 
-    public void Recovery()
-    {
-        shieldCount = aIs.Count;
-    }
+    // public void Recovery()
+    // {
+    //     shieldCount = aIs.Count;
+    // }
 
-    public void DecreaseShieldCount()
-    {
-        --shieldCount;
-        if(shieldCount == 0)
-        {
-            foreach(var ai in aIs)
-            {
-                ai.ChangeState(ImmortalJirungE_AI.State.Stun);
+    // public void DecreaseShieldCount()
+    // {
+    //     --shieldCount;
+    //     if(shieldCount == 0)
+    //     {
+    //         foreach(var ai in aIs)
+    //         {
+    //             ai.ChangeState(ImmortalJirungE_AI.State.Stun);
 
-                Recovery();
-            }
+    //             Recovery();
+    //         }
 
-            whenAllShieldDestroy?.Invoke();
-        }
-    }
+    //         whenAllShieldDestroy?.Invoke();
+    //     }
+    // }
 }
