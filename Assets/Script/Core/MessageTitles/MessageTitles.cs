f32ba6c80bd2b983742d6c28bcd6bf267380bc50
Umin Lee <netuee@naver.com>
2021-07-10 17:13:40 +0900

    change short type to ushort type

--- Assets/Script/Core/MessageTitles/MessageTitles.cs
+++ Assets/Script/Core/MessageTitles/MessageTitles.cs
@@ -1,6 +1,6 @@
 
 public static partial class MessageTitles
 {
-    public const short system_registerRequest = 0x0100;
-    public const short system_withdrawRequest = 0x0101;
+    public const ushort system_registerRequest = 0x0100;
+    public const ushort system_withdrawRequest = 0x0101;
 }
