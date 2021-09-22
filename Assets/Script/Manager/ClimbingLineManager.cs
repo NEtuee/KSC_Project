4ac0d11470bd9041ea74005bfd278ea1b5347777
Ym39 <dpals39@gmail.com>
2021-09-22 16:36:14 +0900

    공간분할 노드 코드 추가

--- Assets/Script/Manager/ClimbingLineManager.cs
+++ Assets/Script/Manager/ClimbingLineManager.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CL_Node
+{
+    public Vector3 min = Vector3.zero;
+    public Vector3 max = Vector3.zero;
+
+    public CL_Node[] child = { null, null, null, null };
+    public CL_Node()
+    {
+    }
+    public CL_Node(Vector3 min, Vector3 max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
 public class ClimbingLineManager : ManagerBase
 {
     public List<ClimbingLine> climbingLines = new List<ClimbingLine>();
+    private CL_Node _rootNode = new CL_Node();
+
 
     //public override void Assign()
     //{
@@ -18,6 +35,38 @@ public class ClimbingLineManager : ManagerBase
     //     });
     //}
 
+    public void BulidNode(int minClimbingLineNum)
+    {
+
+    }
+
+    private void DivideNode(CL_Node node, int minClimbingLineNum)
+    {
+        int count = 0;
+        foreach(var line in climbingLines)
+        {
+            if(CheckClimbingLineInNode(node,line))
+            {
+
+            }
+        }
+    }
+
+    private bool CheckClimbingLineInNode(CL_Node node, ClimbingLine line)
+    {
+        Vector3 linePosition = line.transform.position;
+
+        if (node.min.x >= linePosition.x || node.max.x <= linePosition.x)
+            return false;
+
+        if (node.min.y >= linePosition.y || node.max.y <= linePosition.y)
+            return false;
+
+        if (node.min.z >= linePosition.z || node.max.z <= linePosition.z)
+            return false;
+
+        return true;
+    }
     protected override void Awake()
     {
         base.Awake();
