78483d52c522769079d4e483cc46b6d62125fd14
Umin Lee <netuee@naver.com>
2021-11-09 00:59:48 +0900

    drone tilt

--- Assets/Script/Boss/Genie/Phase/Genie_DroneTilt.cs
+++ Assets/Script/Boss/Genie/Phase/Genie_DroneTilt.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Genie_DroneTilt : MonoBehaviour
 {
+    public float tiltDirection = 1f;
+    public float floatingSpeed = 5f;
+    public float floatingFactor = 0.5f;
+
+    public Transform target;
     private Vector3 _prevPosition;
+    private Vector3 _localPosition;
 
     private Vector3 _eulerAngles;
+    private float _sin;
     public void Start()
     {
         _prevPosition = transform.position;
+        _localPosition = target.localPosition;
         _eulerAngles = transform.localEulerAngles;
     }
 
     public void FixedUpdate()
     {
         var factor = _prevPosition - transform.position;
-        transform.localEulerAngles = _eulerAngles + Vector3.right * factor.magnitude * 0.2f;
+        transform.localEulerAngles = _eulerAngles + Vector3.right * factor.magnitude * 1000f;
+
+        if(target.parent != null)
+        {
+            _sin += Time.fixedDeltaTime * floatingSpeed;
+            target.localPosition = _localPosition + Vector3.forward * Mathf.Sin(_sin) * floatingFactor;
+        }
+        
 
         _prevPosition = transform.position;
     }
