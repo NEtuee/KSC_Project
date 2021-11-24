6ab9c7de8c9e1357867a383c42df95c4968d24d0
Ym39 <dpals39@gmail.com>
2021-11-24 21:41:42 +0900

    메인허브 트리거

--- Assets/Script/Stage/MainHub/MainHubStageManager.cs
+++ Assets/Script/Stage/MainHub/MainHubStageManager.cs
@@ -5,10 +5,27 @@ using MD;
 
 public class MainHubStageManager : ObjectBase
 {
+    private string aClearTrigger = "AClear";
+    private string bClearTrigger = "BClear";
+
+    private bool clear;
+
+    public GameObject firstHubTrigger;
+    public GameObject aComeBackTrigger;
+    public GameObject bComeBackTrigger;
+
     public override void Assign()
     {
         base.Assign();
 
+        AddAction(MessageTitles.boolTrigger_getTrigger, (x) =>
+        {
+            var sendData = MessageDataPooling.CastData<MD.TriggerData>(x.data);
+            if (sendData != null)
+            {
+                clear = sendData.trigger;
+            }
+        });
     }
 
     public override void Initialize()
@@ -31,5 +48,23 @@ public class MainHubStageManager : ObjectBase
         var blockQuickStand = MessageDataPooling.GetMessageData<BoolData>();
         blockQuickStand.value = false;
         SendMessageEx(MessageTitles.player_blockQuickStand, GetSavedNumber("Player"), blockQuickStand);
+
+        var data = MessageDataPooling.GetMessageData<MD.StringData>();
+        data.value = aClearTrigger;
+        SendMessageEx(MessageTitles.boolTrigger_getTrigger, GetSavedNumber("GlobalTriggerManager"), data);
+
+        if(clear == true)
+        {
+            firstHubTrigger.SetActive(false);
+        }
+
+        var data2 = MessageDataPooling.GetMessageData<MD.StringData>();
+        data2.value = bClearTrigger;
+        SendMessageEx(MessageTitles.boolTrigger_getTrigger, GetSavedNumber("GlobalTriggerManager"), data2);
+
+        if (clear == true)
+        {
+            aComeBackTrigger.SetActive(false);
+        }
     }
 }
